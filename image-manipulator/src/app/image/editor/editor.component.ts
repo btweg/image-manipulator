97caebe2ce7ffa3d9ef2386faf537ce9fee08a83
Brooks Wegmann <brooks@Brookss-MacBook-Pro.local>
2019-05-12 13:57:15 -0400

    Draw user input onto canvas element

--- image-manipulator/src/app/image/editor/editor.component.ts
+++ image-manipulator/src/app/image/editor/editor.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+  styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements OnInit, AfterViewInit{
 
+  context: CanvasRenderingContext2D;
+
+  @ViewChild('canvas') canvas: ElementRef;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectFile(fileEvent): void {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const img = new Image();
+      img.onload = () => {
+        // this.canvas.width = img.width;
+        // this.canvas.height = img.height;
+        this.context.drawImage(img, 0, 0,
+                               img.width, img.height, 0, 0,
+                               this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+      };
+
+      img.src = event.target['result'];
+    };
+    reader.readAsDataURL(fileEvent.target.files[0]);
+  }
+
+  ngAfterViewInit() {
+    this.context = (<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d');
   }
 
 }
