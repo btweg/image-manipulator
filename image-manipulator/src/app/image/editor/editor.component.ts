277a2b8e5eb1cc402e944728c92f8bbb8101ca98
Brooks Wegmann <brooks@Brookss-MacBook-Pro.local>
2019-05-12 21:03:25 -0400

    Draw photo to canvas, add sharpen input

--- image-manipulator/src/app/image/editor/editor.component.ts
+++ image-manipulator/src/app/image/editor/editor.component.ts
@@ -9,7 +9,12 @@ export class EditorComponent implements OnInit, AfterViewInit{
 
   context: CanvasRenderingContext2D;
 
+  sharpness = 0;
+
+  originalImage: CanvasImageSource;
+
   @ViewChild('canvas') canvas: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
@@ -28,12 +33,35 @@ export class EditorComponent implements OnInit, AfterViewInit{
       };
 
       img.src = event.target['result'];
+      this.originalImage = img;
     };
     reader.readAsDataURL(fileEvent.target.files[0]);
   }
 
   ngAfterViewInit() {
     this.context = (<HTMLCanvasElement>this.canvas.nativeElement).getContext('2d');
+  }
+
+  applyFilters() {
+    // tslint:disable-next-line: max-line-length
+    const imageData = this.context.getImageData(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+    let newImageData: ImageData;
+
+    // tslint:disable-next-line: max-line-length
+    newImageData = new ImageData(this.sharpenImage(imageData.data, this.sharpness), this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+
+    this.context.putImageData(newImageData, 0, 0);
+  }
+
+  sharpenImage(imageData: Uint8ClampedArray, level: number): Uint8ClampedArray {
+    for (let i = 0; i < imageData.length; i += 1) {
+      imageData[i] = Math.floor(imageData[i] * 1.2);
+      if (imageData[i] > 255) {
+        imageData[i] = 255;
+      }
+    }
+    return imageData;
   }
 
 }
--- image-manipulator/src/app/image/image.module.ts
+++ image-manipulator/src/app/image/image.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditorComponent } from './editor/editor.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [EditorComponent],
   imports: [
     CommonModule,
+    FormsModule,
   ],
   exports: [EditorComponent],
 })
